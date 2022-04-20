1648a009251e3f5cf2063b8588bdf8958d4359ca
dadams28 <99428345+dadams28@users.noreply.github.com>
2022-04-19 20:10:55 -0400

    Add files via upload

--- CS321-Team2-Project/src/FileOpenAndFormat.java
+++ CS321-Team2-Project/src/FileOpenAndFormat.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FileOpenAndFormat {
-
-	
 	
 	public File openFile() {
 		File foodList = new File("foodList.csv");
@@ -32,19 +30,17 @@ public class FileOpenAndFormat {
 		ArrayList<FoodItem> foodList = new ArrayList<>();
 		
 		for (int x = 0; x < list.size(); x++) {
+			
 			String[] lineSplit = list.get(x).split(",");
-			List<String> lineSplitList = Arrays.asList(lineSplit);
-			List<String> ingredients = lineSplitList.subList(3, lineSplitList.size());
+			ArrayList<String> lineSplitList = (ArrayList<String>)Arrays.asList(lineSplit);
+			
+			ArrayList<String> ingredients = (ArrayList<String>)lineSplitList.subList(3, lineSplitList.size());
+			
 			FoodItem temp = new FoodItem(lineSplitList.get(0), Integer.parseInt(lineSplitList.get(1)),Double.parseDouble(lineSplitList.get(2)), ingredients);
+			
 			foodList.add(temp);
 		}
 		
 		return foodList;
 	}
-	
-	public static void main(String[] args) {
-		
-
-	}
-
 }
