74ed3945ade62a50e1f3dfea8f9afcbbc37c2815
tjrhulley <57766828+tjrhulley@users.noreply.github.com>
2022-04-26 15:41:38 -0400

    Add files via upload

--- CS321-Team2-Project/src/PlanOrganizer.java
+++ CS321-Team2-Project/src/PlanOrganizer.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class PlanOrganizer {
 	private ArrayList<FoodItem> foodList;
@@ -8,6 +9,7 @@ public class PlanOrganizer {
 	private int calorieLimit;
 	private int proteinLimit;
 	private ArrayList<String> allergyList;
+	private ArrayList<FoodItem> mealPlan;
 	
 	PlanOrganizer () {
 		foodList = new ArrayList<FoodItem>();
@@ -19,7 +21,7 @@ public class PlanOrganizer {
 		allergyList = new ArrayList<String>();
 	}
 	
-	void getFoodList(ArrayList<FoodItem> foodList) {
+	void setFoodList(ArrayList<FoodItem> foodList) {
 		this.foodList = foodList;
 	}
 	
@@ -31,13 +33,21 @@ public class PlanOrganizer {
 		this.proteinLimit = protein;
 	}
 	
+	void setAllergies(ArrayList<String> allergyList) {
+		this.allergyList = allergyList;
+	}
+	
+	ArrayList<FoodItem> getMealPlan() {
+		return mealPlan;
+	}
+	
 	boolean checkAllergies(FoodItem food) {
 		if (allergyList.isEmpty()) {
 			return true;
 		}
 		
 		for (int i = 0; i < allergyList.size(); i++) {
-			if (food.getIngredients().contains(allergyList.get(1))) {
+			if (food.getIngredients().contains(allergyList.get(i))) {
 				return false;
 			}
 		}
@@ -50,18 +60,74 @@ public class PlanOrganizer {
 			return;
 		}
 		
+		breakList.clear();
+		lunchList.clear();
+		dinnerList.clear();
+		
 		for (int i = 0; i < foodList.size(); i++) {
-			if (foodList.get(i).getTime() == Time.BREAKFAST) {
-				breakList.add(foodList.get(i));
-			} else if (foodList.get(i).getTime() == Time.LUNCH) {
-				lunchList.add(foodList.get(i));
-			} else {
-				dinnerList.add(foodList.get(i));
+			if (checkAllergies(foodList.get(i))) {
+				if (foodList.get(i).getTime() == Time.BREAKFAST) {
+					breakList.add(foodList.get(i));
+				} else if (foodList.get(i).getTime() == Time.LUNCH) {
+					lunchList.add(foodList.get(i));
+				} else {
+					dinnerList.add(foodList.get(i));
+				}
 			}
 		}
 	}
 	
 	void createPlan() {
+		if ((calorieLimit <= 0) || (proteinLimit <= 0)) {
+			return;
+		}
 		
+		Random rand = new Random(System.currentTimeMillis());
+		int calorieTotal = 0;
+		int planCheck = 0;
+		int timeCheck = 0;
+		int food = 0;
+		
+		mealPlan.clear();
+		divideFoods();
+		
+		while (planCheck < 2) {
+			if (timeCheck == 0) {//Breakfast
+				food = rand.nextInt(breakList.size());
+				if ((breakList.get(food).getCalories() + calorieTotal) <= calorieLimit ) {
+					mealPlan.add(breakList.get(food));
+					calorieTotal += breakList.get(food).getCalories();
+				}
+				timeCheck++;
+			} else if (timeCheck == 1) {//Lunch
+				food = rand.nextInt(lunchList.size());
+				if ((lunchList.get(food).getCalories() + calorieTotal) <= calorieLimit ) {
+					mealPlan.add(lunchList.get(food));
+					calorieTotal += lunchList.get(food).getCalories();
+				}
+				timeCheck++;
+			} else {//Dinner
+				food = rand.nextInt(dinnerList.size());
+				if ((dinnerList.get(food).getCalories() + calorieTotal) <= calorieLimit ) {
+					mealPlan.add(dinnerList.get(food));
+					calorieTotal += dinnerList.get(food).getCalories();
+				}
+				timeCheck = 0;
+			}
+			
+			planCheck++;
+			
+			if (planCheck >= 2) {
+				if (calorieTotal < calorieLimit) {
+					planCheck--;
+				}
+			}
+		}
+	}
+	
+	void test() {
+		System.out.println(breakList);
+		System.out.println(lunchList);
+		System.out.println(dinnerList);
 	}
 }
